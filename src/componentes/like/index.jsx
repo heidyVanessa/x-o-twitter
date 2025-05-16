0f15bfd4704aecbec521ca097f5c17784a131d6c
Brayan Camilo <130872257+Lebraiann@users.noreply.github.com>
2025-05-16 19:18:41 +0000

    Co-authored-by: Jhoan-19 <Jhoan-19@users.noreply.github.com>

---
+++ src/componentes/like/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+
+function Like() {
+
+  return (
+    <>
+     <h1>Like</h1>
+    </>
+  )
+}
+
+export default Like
---
+++ src/componentes/rt/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+
+function Rt() {
+
+  return (
+    <>
+     <h1>Rt</h1>
+    </>
+  )
+}
+
+export default Rt
